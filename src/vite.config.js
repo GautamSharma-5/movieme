ff426c9e439b43ea3c8e863c8f5d3e96ac4b89c0
Gautam Sharma <gautamsharma2553@gmail.com>
2025-04-08 17:55:52 +0530

    Delete src/vite.config.js

--- src/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.ts or vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/movieme/', // <-- Your repo name here (must match GitHub!)
-  plugins: [react()],
-})
